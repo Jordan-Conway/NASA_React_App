203b8b3dd710f7997bd21b19eed432167c40098e
Jordan-Conway <D00244954@student.dkit.ie>
2025-06-16 12:50:14 +0100

    Add .env to backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,9 @@
 const express = require("express")
+require("dotenv").config()
 
 const app = express()
 
 app.listen(8080, () => {
     console.log("Server listening in port 8080")
+    console.log(process.env.NASA_API_KEY)
 })
